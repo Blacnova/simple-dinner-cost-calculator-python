7de97fbf7e622eb19a47c875583a1def889ba9d5
Patrick Kenny <Didey@users.noreply.github.com>
2017-03-19 01:23:30 -0400

    Update tip_tax_calc.py
    Removes unneeded code, makes code run better(no unneeded flushing...), reads easier, adds comments, removes unneeded imports, cleans code.

--- tip_tax_calc.py
+++ tip_tax_calc.py
@@ -1,34 +1,17 @@
-from time import sleep
-import sys
-import msvcrt
 
+# Find the initial price of the meal and convert to a float.
 mealprice = float(input("Enter the price of your dinner this evening: "))
 
+# Do the same for the tax.
 tax = float(input("Thanks.\nEnter the tax (decimal form): "))
 
+# And the tip.
 tip = float(input("Last step, I promise.\nHow much are you willing to cough up to the waiter for his service (decimal form)? "))
 
+# Calculate the final price of the meal, including the tip and the tax.(Although tip should be calculated before tax, to be cheap.)
 mealprice = mealprice + mealprice * tax
 mealprice = mealprice + mealprice * tip
 
-
-print("Calculating", end="")
-sys.stdout.flush()
-sleep(1)
-
-print(".", end="")
-sys.stdout.flush()
-sleep(1)
-
-print(".", end="")
-sys.stdout.flush()
-sleep(1)
-
-print(".")
-
-
-print("Thanks for using this calculator, your meal will cost you %s, hope it wasn't too expensive!" % (mealprice))
-sys.stdout.flush()
-sleep(3)
-print("Press any key to close!")
-msvcrt.getch()
+print("Thanks for using this calculator, your meal will cost you {}, hope it wasn't too expensive! \n Press enter to close.".format(mealprice))
+# Waits for enter in a much, much more efficient way.
+input("")
